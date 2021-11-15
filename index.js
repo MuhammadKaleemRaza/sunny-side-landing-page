b401d7da1cb22a7fb967aa917e43fda60e342eb2
kaleem <muhammadkaleemraza58@gmail.com>
2021-11-15 15:57:37 +0500

    final commit after making the site responsive

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let $burger = document.getElementById('burger');
 let $navList = document.getElementById('nav-list');
 
-$burger.addEventListener('click', function() {
+$burger.addEventListener('click', function () {
     $navList.classList.toggle('v-height-resp')
 })
